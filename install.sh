75074e041f00595bd61b01d494e34ea919f420a9
Daniil Molkov <daniil.molkov@flant.com>
2024-10-28 15:52:56 +0100

    Added plugin manager and minor fixes

--- install.sh
+++ install.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/bash
+
+vim_install_plug() {
+    echo 'Download pluging manager'
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 2> /dev/null
+    echo 'Install plugins'
+    vim +PlugInstall +qall
+}
+
 install() {
     local SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     local SETTINGS_PATH="${SCRIPT_DIR}/settings"
     # Check if Neovim is installed
     if command -v nvim &> /dev/null; then
-        echo "Neovim is found"
+        echo 'Neovim is found'
         local NVIM_CONFIG_DIR="${HOME}/.config/nvim"
         # Ensure Neovim's config directory exists
         mkdir -p "$NVIM_CONFIG_DIR"
         # Copy .vimrc to Neovim's init.vimi
         echo "Copying ${SETTINGS_PATH} to ${NVIM_CONFIG_DIR}/init.vim"
-        cp "${SETTINGS_PATH}" "${NVIM_CONFIG_DIR}/init.vim"
+        cp -i "${SETTINGS_PATH}" "${NVIM_CONFIG_DIR}/init.vim"
     elif command -v vim &> /dev/null; then
-        echo "Vim is found"
+        echo 'Vim is found'
         echo "Copying from ${SETTINGS_PATH} to ${HOME}/.vimrc"
-        cp ${SETTINGS_PATH} "${HOME}/.vimrc"
+        cp -i ${SETTINGS_PATH} "${HOME}/.vimrc"
+        vim_install_plug
     else
         echo "Error: Neither Neovim nor Vim is installed."
         return 1
